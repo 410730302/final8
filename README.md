aaff213d58a27f77785b82cce79838f2975a06b1
410730302 <131839833+410730302@users.noreply.github.com>
2023-05-30 14:40:01 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pumpkin Slider Landing Page
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/LEE-I-CHI/pen/bGmJMMo](https://codepen.io/LEE-I-CHI/pen/bGmJMMo).
+
+Based on this figma design: https://www.figma.com/file/NQXjkByRI5wJtRUgFO9Yr7/Ghost-Agency---Ghost-Illustration-Kit-(Community)?node-id=2%3A115
---
+++ script.js
@@ -0,0 +1,249 @@
+var vm = new Vue({
+    el: "#app",
+    data: {
+      keyword: "",
+      cards: [
+        {
+          title: "李翊琪",
+          cover:
+            "https://cdn.mos.cms.futurecdn.net/wtqqnkYDYi2ifsWZVW2MT4-1200-80.jpg",
+          address: "淡江大學 教育科技學系",
+          tel: "看日劇、看美劇",
+          opentime: "無尾熊（Koala）",
+          title_url: "https://www.facebook.com/IlluminationBooks/",
+          bgimage: ""
+        },
+        ],
+
+        cards5: [
+          {
+          bgimage: ""
+        },
+        ],
+
+        cards2: [
+          {
+            bgimage: ""
+          },
+          ],
+
+
+          cards3: [
+            {
+              bgimage: ""
+            },
+            ],
+
+
+          cards4: [
+            {
+              bgimage: ""
+            },
+            ],
+
+          cards6: [
+            {
+            bgimage: ""
+          },
+          ],
+
+          cards7: [
+            {
+              bgimage: ""
+            },
+            ],
+    
+
+    },
+    
+    mounted:function(){        
+        const swiper = new Swiper('.carousel', {
+          loop: true,
+            speed: 1000,
+            autoplay: {
+                delay: 3000,
+            },
+            effect: 'coverflow',
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: 'auto',
+            coverflowEffect: {
+                rotate: 0,
+                stretch: 80,
+                depth: 200,
+                modifier: 1,
+                slideShadows: false,
+            },    
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev'
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                type: "fraction",
+            }
+        });  
+
+        }
+  });
+
+
+
+
+
+
+
+
+
+/* SHOW MENU */
+const navMenu = document.getElementById("nav-menu"),
+	navToggle = document.getElementById("nav-toggle"),
+	navClose = document.getElementById("nav-close");
+
+/* MENU SHOW */
+/* Validate if constant exists */
+if (navToggle) {
+	navToggle.addEventListener("click", () => {
+		navMenu.classList.add("show-menu");
+	});
+}
+
+/* MENU HIDDEN */
+/* Validate if constant exists */
+if (navClose) {
+	navClose.addEventListener("click", () => {
+		navMenu.classList.remove("show-menu");
+	});
+}
+
+/* REMOVE MENU MOBILE */
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+	const navMenu = document.getElementById("nav-menu");
+	// When we click on each nav__link, we remove the show-menu class
+	navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/* HOME SWIPER */
+let homeSwiper = new Swiper(".home-swiper", {
+	spaceBetween: 30,
+	loop: "true",
+
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true
+	}
+});
+
+/* change body's background color */
+
+let root = document.documentElement;
+
+homeSwiper.on("transitionEnd", function (e) {
+	if (this.activeIndex == 1) {
+		root.style.setProperty(
+			"--body-color",
+			"linear-gradient(to right, #5B874B, #0C3720)"
+		);
+		root.style.setProperty("--sub", "#ffffff");
+		root.style.setProperty("--title-color", "#ffffff");
+		root.style.setProperty(
+			"--container-color",
+			"linear-gradient(136deg, #5B874B, #0C3720)"
+		);
+	}
+	if (this.activeIndex == 2) {
+		root.style.setProperty(
+			"--body-color",
+			"linear-gradient(to right, #E8CAFB, #6A4FB6)"
+		);
+		root.style.setProperty("--sub", "#303056");
+		root.style.setProperty("--title-color", "#303056");
+		root.style.setProperty(
+			"--container-color",
+			"linear-gradient(136deg, #E8CAFB, #6A4FB6)"
+		);
+	}
+	if (this.activeIndex == 3) {
+		root.style.setProperty(
+			"--body-color",
+			"linear-gradient(to right, #2E0916, #200A2B)"
+		);
+		root.style.setProperty("--sub", "#ff5b79");
+		root.style.setProperty("--title-color", "#ffffff");
+		root.style.setProperty(
+			"--container-color",
+			"linear-gradient(136deg, #2E0916, #200A2B)"
+		);
+	}
+});
+
+/* CHANGE BACKGROUND HEADER */
+function scrollHeader() {
+	const header = document.getElementById("header");
+	// When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+	if (this.scrollY >= 50) header.classList.add("scroll-header");
+	else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+/* NEW SWIPER */
+let newSwiper = new Swiper(".new-swiper", {
+	centeredSlides: true,
+	slidesPerView: "auto",
+	loop: "true",
+	spaceBetween: 16
+});
+
+/* SCROLL SECTIONS ACTIVE LINK */
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+	const scrollY = window.pageYOffset;
+
+	sections.forEach((current) => {
+		const sectionHeight = current.offsetHeight,
+			sectionTop = current.offsetTop - 58,
+			sectionId = current.getAttribute("id");
+
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			document
+				.querySelector(".nav__menu a[href*=" + sectionId + "]")
+				.classList.add("active-link");
+		} else {
+			document
+				.querySelector(".nav__menu a[href*=" + sectionId + "]")
+				.classList.remove("active-link");
+		}
+	});
+}
+window.addEventListener("scroll", scrollActive);
+
+/* SHOW SCROLL UP */
+function scrollUp() {
+	const scrollUp = document.getElementById("scroll-up");
+	// When the scroll is higher than 460 viewport height, add the show-scroll class to the a tag with the scroll-top class
+	if (this.scrollY >= 460) scrollUp.classList.add("show-scroll");
+	else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+
+/* SCROLL REVEAL ANIMATION */
+const sr = ScrollReveal({
+	origin: "top",
+	distance: "60px",
+	duration: 2500,
+	delay: 400
+	// reset: true
+});
+
+sr.reveal(`.home-swiper, .new-swiper, .newsletter__container`);
+sr.reveal(`.category__data, .trick__content, .footer__content`, {
+	interval: 100
+});
+sr.reveal(`.about__data, .discount__img`, { origin: "left" });
+sr.reveal(`.about__img, .discount__data`, { origin: "right" });
+
+
